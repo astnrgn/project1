6cf5c452bf491297c2e3fa6f51347d7cba6521c7
Austin Michael Regan <austinmregan@gmail.com>
2019-07-17 16:58:06 -0400

    added to space mode

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var blink = (array) => {
   var i = 0;
   var interval = setInterval(function() {
     $("#" + array[i]).fadeTo(200, 0.4).fadeTo(200, 1);
-    i++;
+    i ++;
   }, timeInterval);
 };
 
@@ -31,7 +31,7 @@ function play() {
   blink(simonPattern);
 
   $(".game-div").off("click").on("click", function(evt) {
-  	evt.preventDefault();
+    evt.preventDefault();
     $(this).fadeTo(200, 0.4).fadeTo(200, 1);
     playerPattern.push($(this).attr("id"));
 
@@ -61,49 +61,81 @@ $("#reset").click(function(evt) {
 
 $("#theme-option-space").click(function(evt) {
   evt.preventDefault();
-    $("body").css( {
-      "background" : "url(images/space-background.gif)",
-      "backgroundSize" : "cover",
-      "backgroundPosition" : "center center",
-      "backgroundRepeat" : "no-repeat"
-    });
+  $("body").css({
+    "background": "url(images/space-background.gif)",
+    "backgroundSize": "cover",
+    "backgroundPosition": "center center",
+    "backgroundRepeat": "no-repeat"
+  });
+  $('.js-space-switch').css("border", "2px solid white");
+  $('.js-space-switch-catagory').css("borderRight", "2px solid white");
+  $('.js-space-switch-catagory').css("color", "white");
 
-    $('.space-switch').css("border", "2px solid white");
-    $('.space-switch-catagory').css("borderRight", "2px solid white");
-    $('.space-switch-catagory').css("color", "white");
+  document.querySelector("#theme-option-space").style.background = "rgb(170,170,170)";
+  document.querySelector("#theme-option-standard").style.background = "white";
+  document.querySelector("header").style.color = "white";
+  document.querySelector("#score-counter").style.background = "black";
 
-    document.querySelector("#theme-option-space").style.background = "rgb(170,170,170)";
-    document.querySelector("#theme-option-standard").style.background = "white";
 
-    document.querySelector("header").style.color = "white";
-
-    document.querySelector("#score-counter").style.background = "black";
+  $("#green").css({
+    "background": "url(images/green-saturn.jpg)",
+    "backgroundSize": "cover",
+    "backgroundPosition": "center center",
+    "backgroundRepeat": "no-repeat"
+  })
+  $("#red").css({
+    "background": "url(images/red-mars.jpg)",
+    "backgroundSize": "cover",
+    "backgroundPosition": "center center",
+    "backgroundRepeat": "no-repeat"
+  })
+  $("#yellow").css({
+    "background": "url(images/yellow-venus.jpg)",
+    "backgroundSize": "cover",
+    "backgroundPosition": "center center",
+    "backgroundRepeat": "no-repeat"
+  })
+  $("#blue").css({
+    "background": "url(images/blue-neptune.jpg)",
+    "backgroundSize": "cover",
+    "backgroundPosition": "center center",
+    "backgroundRepeat": "no-repeat"
+  })
 });
 
 
 $("#theme-option-standard").click(function(evt) {
   evt.preventDefault();
-    $("body").css( {
-      "background" : "white",
-    });
+  $("body").css({
+    "background": "white",
+  });
+  $('.js-space-switch').css("border", "2px solid black");
+  $('.js-space-switch-catagory').css("borderRight", "2px solid black");
+  $('.js-space-switch-catagory').css("color", "black");
 
-    $('.space-switch').css("border", "2px solid black");
-    $('.space-switch-catagory').css("borderRight", "2px solid black");
-    $('.space-switch-catagory').css("color", "black");
+  document.querySelector("#theme-option-standard").style.background = "rgb(170,170,170)";
+  document.querySelector("#theme-option-space").style.background = "white";
+  document.querySelector("header").style.color = "black";
+  document.querySelector("#score-counter").style.background = "white";
 
-    document.querySelector("#theme-option-standard").style.background = "rgb(170,170,170)";
-    document.querySelector("#theme-option-space").style.background = "white";
-
-    document.querySelector("header").style.color = "black";
-
-    document.querySelector("#score-counter").style.background = "white";
+  $("#green").css({
+    "background": "rgba(62, 221, 75, 0.8)"
+  })
+  $("#red").css({
+    "background": "rgba(221, 75, 62, 0.8)"
+  })
+  $("#yellow").css({
+    "background": "rgba(255, 234, 55, 0.8)"
+  })
+  $("#blue").css({
+    "background": "rgba(105, 215, 250, 0.8)"
+  })
 });
-
 
 
 $("#mode-option-hard").click(function(evt) {
   evt.preventDefault();
-  
+
   clearInterval(timeInterval);
   timeInterval = 350;
   console.log(timeInterval);
