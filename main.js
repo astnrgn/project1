90e7f7e528d457f19fcd5dc15687d4e32c711b8c
Austin Michael Regan <austinmregan@gmail.com>
2019-07-17 10:19:02 -0400

    js - random color to computer array

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 var simonStartSquare = document.querySelector("#simon")
 var gameSquares = document.querySelectorAll(".game-div")
+var resetButton = document.querySelector("#reset");
+var possibilities = ["green", "red", "yellow", "blue"]; 
+var simonPatternArray = [];
+var playerPatternArray = [];
+
 
 simonStartSquare.addEventListener("click", function(evt) {
   startGame();
   console.log("game has started");
 });
 
-var startGame = () => {
+
+function startGame() {
+  pattern(simonPatternArray);
+
   for (let i = 0; i < gameSquares.length; i++) {
     gameSquares[i].addEventListener("click", function(evt) {
       evt.preventDefault();
       $(this).fadeTo(150, 0.4).fadeTo(150, 1);
+      playerPatternArray.push(this.id);
+      console.log(playerPatternArray);
     })
   }
 };
+
+function pattern(array) {
+  var randomColor = possibilities[Math.floor(Math.random() * possibilities.length)];
+  array.push(randomColor);
+  console.log(array);
+}
+
+reset.addEventListener("click", function(evt) {
+  location.reload();
+});
+
+
