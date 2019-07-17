5a66bdcd8dce3f44ae586340c9e04c24505b27de
Austin Michael Regan <austinmregan@gmail.com>
2019-07-17 10:37:16 -0400

    added score counter +1

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ var simonStartSquare = document.querySelector("#simon")
 var gameSquares = document.querySelectorAll(".game-div")
 var resetButton = document.querySelector("#reset");
 var possibilities = ["green", "red", "yellow", "blue"]; 
+var scoreCounter = document.querySelector("#score-number");
+var score = scoreCounter.innerHTML;
+
+
 var simonPatternArray = [];
 var playerPatternArray = [];
+
 
 
 simonStartSquare.addEventListener("click", function(evt) {
   startGame();
   console.log("game has started");
 });
+
 
 
 function startGame() {
@@ -21,15 +27,34 @@ function startGame() {
       $(this).fadeTo(150, 0.4).fadeTo(150, 1);
       playerPatternArray.push(this.id);
       console.log(playerPatternArray);
+      check(simonPatternArray, playerPatternArray);
     })
   }
 };
+
+
+
+function check(array1, array2) {
+ if (JSON.stringify(array1) == JSON.stringify(array2)) {
+    console.log("win");
+    score++;
+    scoreCounter.innerHTML = score;
+  } else {
+    console.log("loss.");
+
+  }
+};
+
+
 
 function pattern(array) {
   var randomColor = possibilities[Math.floor(Math.random() * possibilities.length)];
   array.push(randomColor);
   console.log(array);
-}
+};
+
+
+
 
 reset.addEventListener("click", function(evt) {
   location.reload();
